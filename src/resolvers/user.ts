f29bfc8c899a95c5f95214e1a1b07a4e6e0fb07b
Marvin Gay <MarvinJGay@protonmail.com>
2020-08-24 15:48:00 -0400

    Setup Redis, added sessions

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Resolver, InputType, Mutation, Field, Arg, Ctx, ObjectType } from "type-graphql";
+import { Resolver, InputType, Mutation, Field, Arg, Ctx, ObjectType, Query } from "type-graphql";
 import { MyContext } from "src/types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
@@ -32,10 +32,25 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(
+    @Ctx() { req, em }: MyContext
+  ) {
+    // You are not logged in!
+    if (!req.session.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId })
+
+    return user;
+  }
+
+
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -83,13 +98,18 @@ export class UserResolver {
 
       }
     }
+
+    // Store user id session
+    // Set cookie - keep logged in
+    req.session.userId = user.id;
+
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -112,6 +132,8 @@ export class UserResolver {
         ]
       }
     }
+
+    req.session.userId = user.id;
 
     return {
       user
