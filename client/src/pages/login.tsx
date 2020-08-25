77938df32b981ba2d34f5db7a5e17417c2e1d932
Marvin Gay <MarvinJGay@protonmail.com>
2020-08-25 14:33:20 -0400

    Added components, queries, mutations for Navbar

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -3,7 +3,7 @@ import { Formik, Form } from 'formik'
 import { Box, Button } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
-import { useRegisterMutation } from '../generated/graphql';
+import { useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from "next/router";
 
@@ -11,31 +11,31 @@ interface registerProps {
 
 }
 
-const Register: React.FC<registerProps> = ({ }) => {
+const Login: React.FC<{}> = ({ }) => {
   const router = useRouter()
-  const [{ }, register] = useRegisterMutation();
+  const [{ }, login] = useLoginMutation();
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ username: '', password: '' }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
-          if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors))
-          } else if (response.data?.register.user) {
+          const response = await login({ options: values });
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors))
+          } else if (response.data?.login.user) {
             // Successful registration
             router.push('/');
 
           }
         }}
       >
-        {({ isSubmitting }) => (
+        {({ values, handleChange, isSubmitting }) => (
           <Form>
             <InputField label='Username' name='username' placeholder='username' />
             <Box mt={4}>
               <InputField label='Password' name='password' placeholder='password' type='password' />
             </Box>
-            <Button mt={4} isLoading={isSubmitting} type='submit' variantColor='teal'>Register</Button>
+            <Button mt={4} isLoading={isSubmitting} type='submit' variantColor='teal'>Login</Button>
           </Form>
         )}
       </Formik>
@@ -43,4 +43,4 @@ const Register: React.FC<registerProps> = ({ }) => {
   )
 }
 
-export default Register;
+export default Login;
