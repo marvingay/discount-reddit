e9d83493170ebd804821dc5243d221db4717648f
Marvin Gay <MarvinJGay@protonmail.com>
2020-08-29 15:39:38 -0400

    added field permission on email visibility

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Resolver, Mutation, Field, Arg, Ctx, ObjectType, Query } from "type-graphql";
+import { Resolver, Mutation, Field, Arg, Ctx, ObjectType, Query, FieldResolver, Root } from "type-graphql";
 import { MyContext } from "src/types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
@@ -28,8 +28,18 @@ class UserResponse {
 
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+  @FieldResolver(() => String)
+  email(@Root() user: User, @Ctx() { req }: MyContext) {
+    // current user, show own email
+    if (req.session.userId === user.id) {
+
+      return user.email
+    }
+    // current user, wants other emails?
+    return "";
+  }
 
   @Mutation(() => UserResponse)
   async changePassword(
