9ded8238ddc2da51784b69318f2d359835317336
Marvin Gay <MarvinJGay@protonmail.com>
2020-08-28 22:34:28 -0400

    Added styles, query for main page/posts

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,24 +2,46 @@ import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import Layout from "../components/Layout";
-import { Link } from "@chakra-ui/core";
+import { Link, Stack, Box, Heading, Text, Flex, Button } from "@chakra-ui/core";
 import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     }
   });
+
+  if (!fetching && !data) {
+    return <div>We got nothin' for ya. Somethin' went wrong. Mistakes were made.</div>
+  }
+
   return (
     <Layout>
-      <NextLink href='/create-post'>
-        <Link>
-          Create Post
+      <Flex align='center'>
+        <Heading>Discount Reddit</Heading>
+        <NextLink href='/create-post'>
+          <Link ml='auto'>
+            Create Post
         </Link>
-      </NextLink>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? <div>loading...</div> : data.posts.map(p => <div key={p.id}>{p.title}</div>)}
+      {!data && fetching ? <div>loading...</div> : (
+        <Stack spacing={8}>
+          {data!.posts.map(p => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px" >
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+      {/* Only show Load More button for pagination if data */}
+      {data ? (
+        <Flex>
+          <Button m='auto' my={8}>Load more</Button>
+        </Flex>) : null}
     </Layout>
 
   )
