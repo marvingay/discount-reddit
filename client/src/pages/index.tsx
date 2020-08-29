e8d3247475e00ac5157aac9bf590da3e0894403a
Marvin Gay <MarvinJGay@protonmail.com>
2020-08-29 14:39:15 -0400

    change post pagination to not allow load more when no data

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,7 +7,7 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [variables, setVariables] = useState({ limit: 10, cursor: null as null | string });
+  const [variables, setVariables] = useState({ limit: 33, cursor: null as null | string });
   const [{ data, fetching }] = usePostsQuery({
     variables
   });
@@ -29,7 +29,7 @@ const Index = () => {
       <br />
       {!data && fetching ? <div>loading...</div> : (
         <Stack spacing={8}>
-          {data!.posts.map(p => (
+          {data!.posts.posts.map(p => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px" >
               <Heading fontSize="xl">{p.title}</Heading>
               <Text mt={4}>{p.textSnippet}</Text>
@@ -37,14 +37,14 @@ const Index = () => {
           ))}
         </Stack>
       )}
-      {/* Only show Load More button for pagination if data */}
-      {data ? (
+      {/* Only show Load More button for pagination if data AND not at end*/}
+      {data && data.posts.hasMore ? (
         <Flex>
           <Button m='auto' my={8} onClick={() => {
             setVariables(
               {
                 limit: variables.limit,
-                cursor: data.posts[data.posts.length - 1].createdAt
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt
               }
             )
           }}>Load more</Button>
