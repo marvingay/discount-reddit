674923b0227c270e96ecfad5db4f7b55622b93ee
Marvin Gay <MarvinJGay@protonmail.com>
2020-08-29 19:35:56 -0400

    invalidate cache on createPost  mutation

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,7 +7,10 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [variables, setVariables] = useState({ limit: 33, cursor: null as null | string });
+  const [variables, setVariables] = useState({
+    limit: 15,
+    cursor: null as null | string
+  });
   const [{ data, fetching }] = usePostsQuery({
     variables
   });
@@ -32,6 +35,7 @@ const Index = () => {
           {data!.posts.posts.map(p => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px" >
               <Heading fontSize="xl">{p.title}</Heading>
+              <Text>posted by {p.creator.username}</Text>
               <Text mt={4}>{p.textSnippet}</Text>
             </Box>
           ))}
